bab3e45464852ba3d5565c428d23ae0cc3e8448b
Xhulio <xhulioxole@gmail.com>
2016-02-03 17:21:45 +0100

    Added parse push notification !

--- www/js/app.js
+++ www/js/app.js
@@ -6,14 +6,18 @@ angular.module('flamingoApp',
   ]
 )
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, ParsePush) {
   $ionicPlatform.ready(function() {
+
     if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
     }
+
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
+
+    ParsePush.init();
   });
-})
+});
---
+++ www/js/factories/ParsePush.js
@@ -0,0 +1,54 @@
+/**
+ * Author: Xhulio
+ * Created Date: 2016-01-22 15:22.MD
+ * This code is copyright (c) 2016 Prius Solution
+ */
+
+angular.module('flamingoApp').factory('ParsePush', ['$state', function ($state) {
+  var ParsePush = {};
+  var constants = {
+    APPLICATION_ID: "EeTw0sCimqaTb73PSqvtl942prQh0pPxDzOX0KXl",
+    CLIENT_KEY: "GA3HZCyosVLAamPWsIDSHaCpbtR52x1Ay2oCJRu3",
+    DEFAULT_CHANNEL: "flamingo"
+  };
+  var parseConfig = {
+    appId: constants.APPLICATION_ID,
+    clientKey: constants.CLIENT_KEY,
+    ecb: "onNotification"
+  };
+
+  /**
+   * Init
+   */
+  ParsePush.init = function() {
+    ParsePushPlugin.register(parseConfig, function() {
+      ParsePushPlugin.getInstallationId(function(id) {
+        ParsePushPlugin.subscribe(constants.DEFAULT_CHANNEL, function() {
+          console.log("Subscribed!");
+        }, function(e) {
+          console.log('Error subscribing!');
+        });
+      }, function(e) {
+        console.log('Error getting installationId!');
+      });
+      handlePushNotification();
+    }, function(e) {
+      console.log('Error registering device: ' + e);
+    });
+
+    /**
+     * Open view depending on data
+     */
+    function handlePushNotification() {
+      ParsePushPlugin.received(function(data) {
+        if (data.length > 0) {
+          alert("Received");
+        }
+      }, function(e) {
+        console.log('Error Obtaining Push Data: ' + e);
+      });
+    }
+  };
+
+  return ParsePush;
+}]);
